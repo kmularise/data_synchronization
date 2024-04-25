5beddbc1502926276498ec45e27197218eb5309d
kmularise <kmula222@gmail.com>
2024-04-25 11:50:15 +0900

    fix: mybatis 설정 수정 - mybatis mapper 쿼리 추가

--- src/main/java/factory/integration/database/synchronizer/mapper/target/TargetCustomersDaoMapper.java
+++ src/main/java/factory/integration/database/synchronizer/mapper/target/TargetCustomersDaoMapper.java
@@ -1,15 +1,16 @@
-package factory.integration.database.mapper.source;
+package factory.integration.database.synchronizer.mapper.target;
 
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import factory.integration.database.mapper.CustomerDto;
+import factory.integration.database.synchronizer.mapper.CustomerDto;
 
 @Mapper
-public interface SourceCustomersDaoMapper {
+public interface TargetCustomersDaoMapper {
 	CustomerDto selectCustomerById(Long id);
 	List<CustomerDto> selectAll();
+
 	void insertCustomer(CustomerDto customerDto);
 	void deleteCustomerById(Long id);
 	void updateCustomerById(CustomerDto customerDto);
