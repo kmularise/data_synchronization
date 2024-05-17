8afce9cd8e48e54cb4281010697ce49a1eba9b26
kmularise <kmula222@gmail.com>
2024-05-17 16:15:20 +0900

    chore: target 테이블 not null 조건 제거

--- src/main/resources/sql/schema/temporary_target_tables.sql
+++ src/main/resources/sql/schema/temporary_target_tables.sql
@@ -2,18 +2,18 @@ CREATE TABLE IF_orders
 (
     id          BIGINT,
     count       INT COMMENT '수량',
-    car_id      BIGINT NOT NULL COMMENT '자동차 외래키',
-    customer_id BIGINT NOT NULL COMMENT '주문 고객 외래키'
+    car_id      BIGINT COMMENT '자동차 외래키',
+    customer_id BIGINT COMMENT '주문 고객 외래키'
 );
 CREATE TABLE IF_customers
 (
     id   BIGINT,
-    name VARCHAR(40) NOT NULL COMMENT '이름'
+    name VARCHAR(40) COMMENT '이름'
 );
 
 CREATE TABLE IF_cars
 (
     id    BIGINT,
-    name  VARCHAR(40) NOT NULL COMMENT '이름',
-    price BIGINT      NOT NULL COMMENT '자동차 가격'
+    name  VARCHAR(40) COMMENT '이름',
+    price BIGINT COMMENT '자동차 가격'
 );
