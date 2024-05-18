3d7d411d14c49ce0f737c7c8ae724f4bac9ea3a8
kmularise <kmula222@gmail.com>
2024-05-18 20:00:03 +0900

    chore: 데이터베이스 테스트 테이블 이름 수정

--- src/main/resources/sql/schema/source_tables.sql
+++ src/main/resources/sql/schema/source_tables.sql
@@ -1,16 +1,19 @@
-CREATE TABLE orders(
-                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                       count INT COMMENT '수량',
-                       car_id BIGINT NOT NULL COMMENT '자동차 외래키',
-                       customer_id BIGINT NOT NULL COMMENT '주문 고객 외래키'
+CREATE TABLE car_order
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    count       INT COMMENT '수량',
+    car_id      BIGINT NOT NULL COMMENT '자동차 외래키',
+    customer_id BIGINT NOT NULL COMMENT '주문 고객 외래키'
 );
-CREATE TABLE customers(
-                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          name VARCHAR(40) NOT NULL COMMENT '이름'
+CREATE TABLE customer
+(
+    id   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(40) NOT NULL COMMENT '이름'
 );
 
-CREATE TABLE cars(
-                     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                     name VARCHAR(40) NOT NULL COMMENT '이름',
-                     price BIGINT NOT NULL COMMENT '자동차 가격'
+CREATE TABLE car
+(
+    id    BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name  VARCHAR(40) NOT NULL COMMENT '이름',
+    price BIGINT      NOT NULL COMMENT '자동차 가격'
 );
--- src/main/resources/sql/schema/target_tables.sql
+++ src/main/resources/sql/schema/target_tables.sql
@@ -1,17 +1,17 @@
-CREATE TABLE orders
+CREATE TABLE car_order
 (
     id          BIGINT PRIMARY KEY,
     count       INT COMMENT '수량',
     car_id      BIGINT COMMENT '자동차 외래키',
     customer_id BIGINT COMMENT '주문 고객 외래키'
 );
-CREATE TABLE customers
+CREATE TABLE customer
 (
     id   BIGINT PRIMARY KEY,
     name VARCHAR(40) COMMENT '이름'
 );
 
-CREATE TABLE cars
+CREATE TABLE car
 (
     id    BIGINT PRIMARY KEY,
     name  VARCHAR(40) COMMENT '이름',
